f5535dde288087ec1407065691d5cd529d74bf1b
Gustavo Duarte <gduartemenezes@gmail.com>
2025-02-11 10:28:46 -0300

    feat: update app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { ChatModule } from './chat/chat.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [UserModule, ChatModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017'),
+    UserModule,
+    ChatModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
